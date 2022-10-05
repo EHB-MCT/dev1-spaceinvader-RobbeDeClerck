b6f4a8e97badd2ba9151bac544beaa35f90acc76
Robbe De Clerck <robbe.de.clerck@student.ehb.be>
2022-10-05 19:42:09 +0200

    Space Invader

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,38 @@
 "use strict";
 
-alert("Draw your space invader here");
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+drawSpaceInvader();
+
+function drawSpaceInvader(){
+    context.beginPath();
+    context.lineWidth = 0;
+    context.fillStyle = "black";
+    context.rect(10, 10, 300, 300);
+    context.fill();
+
+
+    context.beginPath();
+    context.fillStyle = "#00FF00";
+    context.rect(40, 40, 50, 50);
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle = "#00FF00";
+    context.rect(240, 40, 50, 50);
+    context.rect(140, 90, 50, 50);
+    context.rect(40, 140, 50, 50);
+    context.rect(90, 140, 50, 50);
+    context.rect(90, 190, 50, 50);
+    context.rect(40, 240, 50, 50);
+    context.rect(140, 240, 50, 50);
+    context.rect(240, 240, 50, 50);
+    context.rect(190, 190, 50, 50);
+    context.rect(190, 140, 50, 50);
+    context.rect(240, 140, 50, 50);
+    context.fill();
+
+}
